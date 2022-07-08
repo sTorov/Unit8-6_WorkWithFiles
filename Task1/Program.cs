edce1b0cc636cdcef20060c8d94ec40706db62b7
sTorov <sotorov2011@yandex.ru>
2022-07-09 00:36:34 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+
+            Console.WriteLine("Укажите путь к папке:");
+            path = Console.ReadLine();
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            if (dir.Exists)
+            {
+                GetDirs(dir);
+            }
+            else
+                Console.WriteLine("NaN");
+
+        }
+        static void GetDirs(DirectoryInfo directoryInfo)
+        {
+            DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                Console.WriteLine(dir.Name);
+                GetDirs(dir);
+            }
+            FileInfo[] files = directoryInfo.GetFiles();
+            foreach (var file in files)
+            {
+                Console.WriteLine("\t" + file.Name);
+            }
+        }
+    }
+}
