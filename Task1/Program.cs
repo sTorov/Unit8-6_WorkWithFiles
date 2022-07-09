097f5991ec627130c8bbccb180e73e38f9332b92
sTorov <sotorov2011@yandex.ru>
2022-07-09 05:46:52 +0300

    Task1 Добавление различных режимов удаления
    Доработка вывода информации на консоль

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -13,40 +13,76 @@ namespace Task1
             string path = string.Empty;
 
             Console.WriteLine("Укажите путь к папке:");
-            path = @"D:\!!!Test";
+            path = Console.ReadLine();
+            Console.WriteLine();
 
             DirectoryInfo dir = new DirectoryInfo(path);
 
-            try
+            if(dir.Exists)
             {
-                DelFile_30min(dir);
-                DelDir_30min(dir);
+                Console.WriteLine("Выберете режим удаления\n1. Не удалять не пустые директории\n2. Ручной выбор\n3. Удалять все автоматически\n4. Удалить только файлы");
+                try
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("\nНажмите '1' '2' '3' '4' соответственно");
+                        var click = Console.ReadKey(true).KeyChar;
+
+                        switch (click)
+                        {
+                            case '1':
+                                DelFile_30min(dir);
+                                DelDir_30min_NoDel(dir);
+                                break;
+                            case '2':
+                                DelFile_30min(dir);
+                                DelDir_30min_Manual(dir);
+                                break;
+                            case '3':
+                                DelFile_30min(dir);
+                                DelDir_30min_Auto(dir);
+                                break;
+                            case '4':
+                                DelFile_30min(dir);
+                                DelDir_30min_FileIn(dir);
+                                break;
+                            default:
+                                continue;
+                        }
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Ошибка: " + ex.Message);
+                    Console.ResetColor();
+                }
             }
-            catch (Exception ex)
+            else
             {
                 Console.BackgroundColor = ConsoleColor.DarkRed;
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Ошибка: " + ex.Message);
+                Console.WriteLine("Такой директории не существует!");
                 Console.ResetColor();
             }
-
         }
-        static void DelDir_30min(DirectoryInfo directoryInfo)
+        static void DelDir_30min_Manual(DirectoryInfo directoryInfo)
         {            
             DirectoryInfo[] dirs = directoryInfo.GetDirectories();
             foreach (var dir in dirs)
             {
                 DelFile_30min(dir);
 
-                if (dir.Exists)
-                    DelDir_30min(dir);
+                DelDir_30min_Manual(dir);
 
                 TimeSpan sub = DateTime.Now - dir.LastAccessTime;
                 if (sub > TimeSpan.FromMinutes(30))
                 {
                     if(dir.GetFiles().Length == 0 && dir.GetDirectories().Length == 0)
                     {
-                        Console.WriteLine(dir.Name + " " + $"Не использовался {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю пустую папку");
+                        Console.WriteLine(dir.Name + " " + $"Не использовалась {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю пустую папку");
                         dir.Delete();
                     }
                     else
@@ -73,6 +109,55 @@ namespace Task1
                 }                
             }
         }
+        static void DelDir_30min_Auto(DirectoryInfo directoryInfo)
+        {
+            DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                DelFile_30min(dir);
+
+                DelDir_30min_Auto(dir);
+
+                TimeSpan sub = DateTime.Now - dir.LastAccessTime;
+                if (sub > TimeSpan.FromMinutes(30))
+                {
+                    Console.WriteLine(dir.Name + " " + $"Не использовалась {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю папку");
+                    dir.Delete(true);
+                }
+            }
+        }
+        static void DelDir_30min_NoDel(DirectoryInfo directoryInfo)
+        {
+            DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                DelFile_30min(dir);
+
+                if (dir.Exists)
+                    DelDir_30min_NoDel(dir);
+
+                TimeSpan sub = DateTime.Now - dir.LastAccessTime;
+                if (sub > TimeSpan.FromMinutes(30))
+                {
+                    if (dir.GetFiles().Length == 0 && dir.GetDirectories().Length == 0)
+                    {
+                        Console.WriteLine(dir.Name + " " + $"Не использовался {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю пустую папку");
+                        dir.Delete();
+                    }
+                }
+            }
+        }
+        static void DelDir_30min_FileIn(DirectoryInfo directoryInfo)
+        {
+            DirectoryInfo[] dirs = directoryInfo.GetDirectories();
+            foreach (var dir in dirs)
+            {
+                DelFile_30min(dir);
+
+                DelDir_30min_FileIn(dir);
+            }
+        }
+
         static void DelFile_30min(DirectoryInfo directoryInfo)
         {
             FileInfo[] files = directoryInfo.GetFiles();
@@ -81,7 +166,7 @@ namespace Task1
                 TimeSpan sub = DateTime.Now - file.LastAccessTime;
                 if (sub > TimeSpan.FromMinutes(30))
                 {
-                    Console.WriteLine(file.FullName + "\t" + $"Не использовался {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю");
+                    Console.WriteLine(file.FullName + "\t" + $"Не использовался {sub.Hours}:{sub.Minutes}:{sub.Seconds}" + "\tУдаляю файл");
                     file.Delete();
                 }
             }            
