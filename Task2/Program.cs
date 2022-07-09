5fd2ea9bc1d6b7ff028155b4aee55509a496f0ad
sTorov <sotorov2011@yandex.ru>
2022-07-10 01:57:28 +0300

    Task2 версия с выводом файлов и папок на консоль

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,16 +4,33 @@
     {
         long size = 0;
         Console.WriteLine("Укажите путь к папке:");
-        string path = @"D:\!!!Test";
+        string path = Console.ReadLine();
         Console.WriteLine();
 
         DirectoryInfo directory = new DirectoryInfo(path);
 
         if(directory.Exists)
         {
-            Console.WriteLine(directory.FullName);
-            size = GetDirSize(directory, ref size);
+            try
+            {
+                size = GetDirSize(directory, ref size);
+            }
+            catch (Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Ошибка: " + ex.Message);
+                Console.ResetColor();
+            }
+
             Console.WriteLine("Размер: " + size + " байт");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Такой директории не существует!");
+            Console.ResetColor();
         }
     }
     static long GetDirSize(DirectoryInfo dir, ref long filesize)
@@ -23,7 +40,10 @@
         var directories = dir.GetDirectories();
 
         foreach (var direct in directories)
+        {
+            Console.WriteLine(direct.Name);
             GetDirSize(direct, ref filesize);
+        }
 
         return filesize;
     }
@@ -32,7 +52,10 @@
         var files = dir.GetFiles();
 
         foreach (var file in files)
+        {
+            Console.WriteLine($"\t{file.Name}\t{file.Length}");
             filesize += file.Length;
+        }
 
         return filesize;
     }
