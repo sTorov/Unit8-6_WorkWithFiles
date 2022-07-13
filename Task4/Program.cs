dfc5c0e8a4771eff2d6967b441bde3fe60351953
sTorov <sotorov2011@yandex.ru>
2022-07-14 01:59:16 +0300

    Task4 Добавление класса Student

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿[Serializable]
+class Student
+{
+    public string Name { get; set; }
+    public string Group { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
+class Program
+{
+    public string dat = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students.dat");
+    static void Main()
+    {
+        string path = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students");
+
+        if(Directory.Exists(path))
+            Directory.CreateDirectory(path);
+    }
+}
