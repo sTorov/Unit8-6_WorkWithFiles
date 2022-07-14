83a8c517f5b6fe6163dc7968f23578a70de2ed3d
sTorov <sotorov2011@yandex.ru>
2022-07-14 03:15:20 +0300

    Переименование Task4 в FinalTask
    Перенос класса Student в отдельный файл

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,18 +1,26 @@
-﻿[Serializable]
-class Student
-{
-    public string Name { get; set; }
-    public string Group { get; set; }
-    public DateTime DateOfBirth { get; set; }
-}
-class Program
-{
-    public string dat = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students.dat");
-    static void Main()
-    {
-        string path = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students");
+﻿using System.Runtime.Serialization.Formatters.Binary;
 
-        if(Directory.Exists(path))
-            Directory.CreateDirectory(path);
+namespace FinalTask
+{    
+    class Program
+    {
+        static void Main()
+        {
+            string dat = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students.dat");
+            string path = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students");
+
+            if (Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var fs = new FileStream(dat, FileMode.OpenOrCreate))
+            {
+                var students = (Student[])bf.Deserialize(fs);
+                foreach(var student in students)
+                {
+                    Console.WriteLine($"{student.Name}\t\t{student.Group}\t{student.DateOfBirth}");
+                }
+            }
+        }
     }
 }
---
+++ Task4/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace FinalTask
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string name, string group, DateTime date)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = date;
+        }
+    }
+}
