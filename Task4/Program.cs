618618ee65b721acd218332b07978675ea49e04b
sTorov <sotorov2011@yandex.ru>
2022-07-14 05:04:20 +0300

    FinalTask Доработка
    Ввод пути к файлу Students.dat с консоли
    Добавление описаний к методам

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,27 +6,92 @@ namespace FinalTask
     {
         static void Main()
         {
-            string dat = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students.dat");
-            string path = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", "Students");
+            Console.WriteLine("Укажите путь к файлу Student.dat:");
 
-            if (Directory.Exists(path))
-                Directory.CreateDirectory(path);
-
-            BinaryFormatter bf = new BinaryFormatter();
-            using (var fs = new FileStream(dat, FileMode.OpenOrCreate))
+            try
             {
-                var students = (Student[])bf.Deserialize(fs);
-                foreach (var student in students)
+                while (true)
                 {
-                    if (!File.Exists(Path.Combine(path, student.Group + ".txt")))
-                        File.Create(Path.Combine(path, student.Group + ".txt"));
+                    string dat = Console.ReadLine();
 
-                    using (var sw = File.AppendText(Path.Combine(path, student.Group + ".txt")))
+                    if (Path.GetFileName(dat) == "Students.dat")
                     {
-                        sw.WriteLine($"{student.Name} {student.DateOfBirth}");
+                        PrintStudentToTxt(dat);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверно указан путь! Попробуйте снова.");
+                        continue;
                     }
                 }
             }
+            catch (Exception e)
+            {
+                PrintException(e);
+                Console.WriteLine("Нажмите любую кнопку для завершения работы приложения");
+            }
+
+
+            Console.ReadKey();
         }
+        /// <summary>
+        /// Дисериализует файл Students.dat. Раскидывает студентов по файлам (каждый файл - отдельная группа), 
+        /// в файле группы студенты перечислены построчно в формате "Имя, дата рождения".
+        /// </summary>
+        /// <param name="dat"></param>
+        static void PrintStudentToTxt(string dat)
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            string dir = CreateDirToFileName(dat);
+
+            using (var fileStream = new FileStream(dat, FileMode.OpenOrCreate))
+            {
+                var students = (Student[])binaryFormatter.Deserialize(fileStream);
+
+                foreach (var student in students)
+                    File.Delete(Path.Combine(dir, student.Group + ".txt"));
+
+                foreach (var student in students)
+                {
+                    if (!File.Exists(Path.Combine(dir, student.Group + ".txt")))
+                    {
+                        using (var stream = File.CreateText(Path.Combine(dir, student.Group + ".txt")))
+                            stream.WriteLine($"{"Имя",-15} Дата рождения\n");
+                    }
+
+                    using (var sw = File.AppendText(Path.Combine(dir, student.Group + ".txt")))
+                        sw.WriteLine($"{student.Name,-15} {student.DateOfBirth:D}");
+                }
+
+                Console.WriteLine("Процесс завершен. Нажмите любую кнопку.");
+            }
+        }
+        /// <summary>
+        /// Создаёт папку на рабочем столе с название, идентичным названию файла. Возвращает путь к этой папке.  
+        /// </summary>
+        /// <param name="dat"></param>
+        /// <returns></returns>
+        static string CreateDirToFileName(string dat)
+        {
+            string dir = Path.Combine(@"C:\Users", Environment.UserName, "Desktop", Path.GetFileNameWithoutExtension(dat));
+
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            return dir;
+        }
+        /// <summary>
+        /// Меняет цвет сообщения об ошибке
+        /// </summary>
+        /// <param name="e"></param>
+        static void PrintException(Exception e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ошибка: " + e.Message);
+            Console.ResetColor();
+        }
+
     }
 }
